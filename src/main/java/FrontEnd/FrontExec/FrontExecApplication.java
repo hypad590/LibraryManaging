e83f07f3deb3fd00528086c9a88324538bb3116e
hypad590 <aloxaryabov@gmail.com>
2024-02-09 16:25:00 +0300

    init

---
+++ src/main/java/FrontEnd/FrontExec/FrontExecApplication.java
@@ -0,0 +1,13 @@
+package FrontEnd.FrontExec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontExecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontExecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FrontEnd/FrontExec/FrontExecApplicationTests.java
@@ -0,0 +1,13 @@
+package FrontEnd.FrontExec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontExecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
